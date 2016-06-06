452d198d187117c794bf4fa953db73830d6b142b
Alan Donohoe <alandonohoe@hotmail.com>
2016-06-06 21:29:11 +0100

    colour out sumoMe promo tab

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,17 @@
 $(function(){
   'use strict';
-    console.log('We Are Red Stars!');
     $('.clock').countdownDigital({
         dateTo: '2016-09-16T16:00',
         labels: false,
         showBlank: true
     });
     // hide sumoMe side bar....
-    $("a[title='SumoMe']").hide();
+    // $('a[title=\'SumoMe\']').hide(); // or....
+    $('a[title=\'SumoMe\']').css('background-color', 'black');
     // hide / show modal
+    // $('a[title=\'SumoMe\']').show();
     // $('.sumome-vex.sumome-popup.sumome-popup-mode-subscribe').show();
     // $('.sumome-vex.sumome-popup.sumome-popup-mode-subscribe').hide();
+
+    // console.log('We Are Red Stars!');
 });
