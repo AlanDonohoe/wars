d1f2a7f5706f9fc110450dd7a2ab9bd6d8079b01
Alan Donohoe <alandonohoe@hotmail.com>
2016-07-28 05:35:11 +0100

    hide clock whilst loading

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,11 @@
 $(function(){
   'use strict';
-  // var noMSecsReqToHideSumoMe = 1500;
+    // var noMSecsReqToHideSumoMe = 1500;
+    // hide / show modal
+    // $('a[title=\'SumoMe\']').show();
+    // $('.sumome-vex.sumome-popup.sumome-popup-mode-subscribe').show();
+    // $('.sumome-vex.sumome-popup.sumome-popup-mode-subscribe').hide();
+
     $('.clock').countdownDigital({
         dateTo: '2016-09-23T20:00',
         labels: false,
@@ -9,10 +14,9 @@ $(function(){
         seperatorChar: ','
     });
 
-    // hide / show modal
-    // $('a[title=\'SumoMe\']').show();
-    // $('.sumome-vex.sumome-popup.sumome-popup-mode-subscribe').show();
-    // $('.sumome-vex.sumome-popup.sumome-popup-mode-subscribe').hide();
-
+    setTimeout(function(){
+        $('#main-container').show();
+    }, 500);
+    
     console.log('We Are Red Stars!');
 });
